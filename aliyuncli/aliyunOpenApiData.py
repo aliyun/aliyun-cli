d22915aca6bf8f1ddbacbec786ed6bca7247b586
feng.changf1 <feng.changf1@alibaba-inc.com>
2017-09-25 17:11:52 +0800

    Using standardized array format.

--- aliyuncli/aliyunOpenApiData.py
+++ aliyuncli/aliyunOpenApiData.py
@@ -360,13 +360,10 @@ class aliyunOpenApiDataHandler():
                 if len(key)>0 and key in keyValues:
                     arg=keyValues[key]
                     if arg is not None and len(arg)>0:
-                        if len(arg) == 1:
+                        if key != 'InstanceIds':
                             param=arg[0]
                         else:
-                            param=[]
-                            for i in range(len(arg)):
-                                if i < len(arg)-1:
-                                    param.append(arg[i])
+                            param=arg
                         getattr(instance,func)(param)
         userKey=self.getUserKey()
         userSecret=self.getUserSecret()
