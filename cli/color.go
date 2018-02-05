53122f0c7f5e3c4fe408289f2e5c7386f4196ca7
ailan.gl <ailan.gl@alibaba-inc.com>
2018-02-05 16:44:58 +0800

    optimize Color formatter

--- cli/color.go
+++ cli/color.go
@@ -3,32 +3,38 @@ package cli
 import "fmt"
 
 const (
-	InfoColor    = "\033[1;34m%s\033[0m"
-	NoticeColor  = "\033[1;36m%s\033[0m"
-	WarningColor = "\033[1;33m%s\033[0m"
-	ErrorColor   = "\033[1;31m%s\033[0m"
-	DebugColor   = "\033[0;36m%s\033[0m"
+	InfoColor    = "\033[1;34m"
+	NoticeColor  = "\033[1;36m"
+	WarningColor = "\033[1;33m"
+	ErrorColor   = "\033[1;31m"
+	DebugColor   = "\033[0;36m"
+	ColorTail = "\033[0m"
 )
 
-func Info(s string) string {
-	return fmt.Sprintf(InfoColor, s)
+func Info(s string) {
+	fmt.Print(InfoColor + s + ColorTail)
 }
 
-func Notice(s string) string {
-	return fmt.Sprintf(NoticeColor, s)
+func Notice(s string) {
+	fmt.Print(NoticeColor + s + ColorTail)
 }
 
-func Warning(s string) string {
-	return fmt.Sprintf(WarningColor, s)
-}
-func Error(s string) string {
-	return fmt.Sprintf(ErrorColor, s)
+func Warning(s string) {
+	fmt.Print(WarningColor + s + ColorTail)
 }
 
-func Debug(s string) string {
-	return fmt.Sprintf(DebugColor, s)
+func Error(s string) {
+	fmt.Print(ErrorColor + s + ColorTail)
 }
 
-func Errorf(msg string, args ...interface{}) {
-	fmt.Printf(Error(msg), args)
+func Debug(s string) {
+	fmt.Print(DebugColor + s + ColorTail)
+}
+
+func Warningf(format string, args ...interface{}) {
+	Warning(fmt.Sprintf(format, args))
+}
+
+func Errorf(format string, args ...interface{}) {
+	Error(fmt.Sprintf(format, args))
 }
