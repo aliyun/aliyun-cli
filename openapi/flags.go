204fa032b2cb8c225d3a02c5438be49dcc9e50f9
jxyowen <jxyowen@ali-6c96cfde97bf.local>
2018-03-28 10:03:38 +0800

    flag description

--- openapi/flags.go
+++ openapi/flags.go
@@ -42,92 +42,92 @@ func AddFlags(fs *cli.FlagSet) {
 
 var (
 	SecureFlag = cli.Flag{Category: "caller",
-	Name: "secure", AssignedMode: cli.AssignedNone,
-	Usage: i18n.T(
-		"use `--secure` to force https",
-		"使用 `--secure` 开关强制使用https方式调用")}
+		Name: "secure", AssignedMode: cli.AssignedNone,
+		Usage: i18n.T(
+			"use `--secure` to force https",
+			"使用 `--secure` 开关强制使用https方式调用")}
 
-    ForceFlag = cli.Flag{Category: "caller",
-	Name: "force", AssignedMode: cli.AssignedNone,
-	Usage: i18n.T(
-		"use `--force` to skip api and parameters check",
-		"添加 `--force` 开关可跳过API与参数的合法性检查")}
+	ForceFlag = cli.Flag{Category: "caller",
+		Name: "force", AssignedMode: cli.AssignedNone,
+		Usage: i18n.T(
+			"use `--force` to skip api and parameters check",
+			"添加 `--force` 开关可跳过API与参数的合法性检查")}
 
-    EndpointFlag = cli.Flag{Category: "caller",
-	Name: "endpoint", AssignedMode: cli.AssignedOnce,
-	Usage: i18n.T(
-		"use `--endpoint <endpoint>` to assign endpoint",
-		"使用 `--endpoint <endpoint>` 来指定接入点地址")}
+	EndpointFlag = cli.Flag{Category: "caller",
+		Name: "endpoint", AssignedMode: cli.AssignedOnce,
+		Usage: i18n.T(
+			"use `--endpoint <endpoint>` to assign endpoint",
+			"使用 `--endpoint <endpoint>` 来指定接入点地址")}
 
-    VersionFlag = cli.Flag{Category: "caller",
-	Name: "version", AssignedMode: cli.AssignedOnce,
-	Usage: i18n.T(
-		"use `--version <YYYY-MM-DD>` to assign product api version",
-		"使用 `--version <YYYY-MM-DD>` 来指定访问的API版本")}
+	VersionFlag = cli.Flag{Category: "caller",
+		Name: "version", AssignedMode: cli.AssignedOnce,
+		Usage: i18n.T(
+			"use `--version <YYYY-MM-DD>` to assign product api version",
+			"使用 `--version <YYYY-MM-DD>` 来指定访问的API版本")}
 
-    HeaderFlag = cli.Flag{Category: "caller",
-	Name: "header", AssignedMode: cli.AssignedRepeatable,
-	Usage: i18n.T(
-		"use `--header X-foo=bar` to add custom HTTP header, repeatable",
-		"使用 `--header X-foo=bar` 来添加特定的HTTP头, 可多次添加")}
+	HeaderFlag = cli.Flag{Category: "caller",
+		Name: "header", AssignedMode: cli.AssignedRepeatable,
+		Usage: i18n.T(
+			"use `--header X-foo=bar` to add custom HTTP header, repeatable",
+			"使用 `--header X-foo=bar` 来添加特定的HTTP头, 可多次添加")}
 
-    BodyFlag = cli.Flag{Category: "caller",
-	Name: "body", AssignedMode: cli.AssignedOnce,
-	Usage: i18n.T(
-		"use `--body $(cat foo.json)` to assign http body in RESTful call",
-		"使用 `--body $(cat foo.json)` 来指定在RESTful调用中的HTTP包体")}
+	BodyFlag = cli.Flag{Category: "caller",
+		Name: "body", AssignedMode: cli.AssignedOnce,
+		Usage: i18n.T(
+			"use `--body $(cat foo.json)` to assign http body in RESTful call",
+			"使用 `--body $(cat foo.json)` 来指定在RESTful调用中的HTTP包体")}
 
-    BodyFileFlag = cli.Flag{Category: "caller",
-	Name: "body-file",AssignedMode: cli.AssignedOnce, Hidden: true,
-	Usage: i18n.T(
-		"assign http body in Restful call with local file",
-		"使用 `--body-file foo.json` 来指定输入包体")}
+	BodyFileFlag = cli.Flag{Category: "caller",
+		Name: "body-file",AssignedMode: cli.AssignedOnce, Hidden: true,
+		Usage: i18n.T(
+			"assign http body in Restful call with local file",
+			"使用 `--body-file foo.json` 来指定输入包体")}
 
-    PagerFlag = cli.Flag{Category: "caller",
-	Name: "all-pages", AssignedMode: cli.AssignedDefault, Hidden: true,
-	Usage: i18n.T(
-		"use `--all-pages` to merge pages for pageable APIs",
-		"使用 `--all-pages` 在访问分页的API时合并分页")}
+	PagerFlag = cli.Flag{Category: "caller",
+		Name: "all-pages", AssignedMode: cli.AssignedDefault, Hidden: true,
+		Usage: i18n.T(
+			"use `--all-pages` to merge pages for pageable APIs",
+			"使用 `--all-pages` 在访问分页的API时合并分页")}
 
-    AcceptFlag = cli.Flag{Category: "caller",
-	Name: "accept", AssignedMode: cli.AssignedOnce, Hidden: true,
-	Usage: i18n.T(
-		"add `--accept {json|xml}` to add Accept header",
-		"使用 `--accept {json|xml}` 来指定Accept头")}
+	AcceptFlag = cli.Flag{Category: "caller",
+		Name: "accept", AssignedMode: cli.AssignedOnce, Hidden: true,
+		Usage: i18n.T(
+			"add `--accept {json|xml}` to add Accept header",
+			"使用 `--accept {json|xml}` 来指定Accept头")}
 
-    OutputTableRowFlag = cli.Flag{Category: "caller",
-	Name: flagOutputTableRows, AssignedMode: cli.AssignedOnce, Hidden: true,
-	Usage: i18n.T(
-		fmt.Sprintf("use `--%s` to assign fields of table row", flagOutputTableRows),
-		fmt.Sprintf("使用 `--%s` 指定表格行的内容", flagOutputTableRows))}
+	OutputTableRowFlag = cli.Flag{Category: "caller",
+		Name: flagOutputTableRows, AssignedMode: cli.AssignedOnce, Hidden: true,
+		Usage: i18n.T(
+			fmt.Sprintf("use `--%s` to assign fields of table row", flagOutputTableRows),
+			fmt.Sprintf("使用 `--%s` 指定表格行的内容", flagOutputTableRows))}
 
-    OutputTableColsFlag = cli.Flag{Category: "caller",
-	Name: flagOutputTableCols, AssignedMode: cli.AssignedOnce, Hidden: true,
-	Usage: i18n.T(
-		fmt.Sprintf("use `--%s` to assign table column names", flagOutputTableCols),
-		fmt.Sprintf("使用 `--%s` 指定表格的列名", flagOutputTableCols))}
+	OutputTableColsFlag = cli.Flag{Category: "caller",
+		Name: flagOutputTableCols, AssignedMode: cli.AssignedOnce, Hidden: true,
+		Usage: i18n.T(
+			fmt.Sprintf("use `--%s` to assign table column names", flagOutputTableCols),
+			fmt.Sprintf("使用 `--%s` 指定表格的列名", flagOutputTableCols))}
 
-    WaitForExprFlag = cli.Flag{Category: "caller",
-	Name: flagWaitForExpr, AssignedMode: cli.AssignedOnce, Hidden: true,
-	Usage: i18n.T(
-		fmt.Sprintf("use `--%s` ", flagWaitForExpr),
-		fmt.Sprintf("使用 `--%s` ", flagWaitForExpr))}
+	WaitForExprFlag = cli.Flag{Category: "caller",
+		Name: flagWaitForExpr, AssignedMode: cli.AssignedOnce, Hidden: true,
+		Usage: i18n.T(
+			fmt.Sprintf("use `--%s` to assign jmes expr", flagWaitForExpr),
+			fmt.Sprintf("使用 `--%s` 指定jmes表达式", flagWaitForExpr))}
 
-    WaitForTargetFlag = cli.Flag{Category: "caller",
-	Name: flagWaitForTarget, AssignedMode: cli.AssignedOnce, Hidden: true,
-	Usage: i18n.T(
-		fmt.Sprintf("use `--%s` ", flagWaitForTarget),
-		fmt.Sprintf("使用 `--%s` ", flagWaitForTarget))}
+	WaitForTargetFlag = cli.Flag{Category: "caller",
+		Name: flagWaitForTarget, AssignedMode: cli.AssignedOnce, Hidden: true,
+		Usage: i18n.T(
+			fmt.Sprintf("use `--%s` to assign target", flagWaitForTarget),
+			fmt.Sprintf("使用 `--%s` 指定目标的值", flagWaitForTarget))}
 
-    WaitTimeoutFlag = cli.Flag{Category: "caller",
-	Name: flagWaitTimeout, AssignedMode: cli.AssignedOnce, Hidden: true,
-	Usage: i18n.T(
-		fmt.Sprintf("use `--%s` ", flagWaitTimeout),
-		fmt.Sprintf("使用 `--%s` ", flagWaitTimeout))}
+	WaitTimeoutFlag = cli.Flag{Category: "caller",
+		Name: flagWaitTimeout, AssignedMode: cli.AssignedOnce, Hidden: true,
+		Usage: i18n.T(
+			fmt.Sprintf("use `--%s` to set timeout(seconds)", flagWaitTimeout),
+			fmt.Sprintf("使用 `--%s` 设定等待超时时间(秒)", flagWaitTimeout))}
 
-    WaitIntervalFlag = cli.Flag{Category: "caller",
-	Name: flagWaitInterval, AssignedMode: cli.AssignedOnce, Hidden: true,
-	Usage: i18n.T(
-		fmt.Sprintf("use `--%s` ", flagWaitInterval),
-		fmt.Sprintf("使用 `--%s` ", flagWaitInterval))}
+	WaitIntervalFlag = cli.Flag{Category: "caller",
+		Name: flagWaitInterval, AssignedMode: cli.AssignedOnce, Hidden: true,
+		Usage: i18n.T(
+			fmt.Sprintf("use `--%s` to set interval(seconds)", flagWaitInterval),
+			fmt.Sprintf("使用 `--%s` 去指定请求间隔时间(秒)", flagWaitInterval))}
 )
