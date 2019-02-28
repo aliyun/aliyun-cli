db42d8f7acfa0340bd0bec5a60b9c88bf316ae73
wenzuochao <wb-wzc505509@alibaba-inc.com>
2019-02-28 09:30:49 +0800

    sdk has removed TZdata

--- cli/platform.go
+++ cli/platform.go
@@ -1,18 +1,11 @@
 package cli
 
 import (
-	"github.com/aliyun/alibaba-cloud-sdk-go/sdk/resource"
-	"github.com/aliyun/alibaba-cloud-sdk-go/sdk/utils"
 	"runtime"
-	"time"
 )
 
 func PlatformCompatible() {
 	if runtime.GOOS == `windows` {
 		DisableColor()
-	}
-	var ok bool
-	if utils.TZData, ok = resource.GetTZData("GMT"); ok {
-		utils.LoadLocationFromTZData = time.LoadLocationFromTZData
 	}
 }
