7e4a5cd2cc6b972b2bc52d6b02599245ab710832
Chaohua Xu <xch971364204@163.com>
2019-06-25 15:50:33 +0800

    update license infomation

--- meta/reader_test.go
+++ meta/reader_test.go
@@ -1,23 +1,37 @@
+// Copyright 1999-2019 Alibaba Group Holding Limited
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package meta
 
 import (
 	"fmt"
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 type reader_test struct {
-	content   string
+	content string
 }
 
-func (r *reader_test)ReadFrom(path string) ([]byte, error){
-	if path == "" || r.content == ""{
+func (r *reader_test) ReadFrom(path string) ([]byte, error) {
+	if path == "" || r.content == "" {
 		return nil, fmt.Errorf("Please insert a valid path.")
 	}
 	return []byte(r.content), nil
 }
 
-func (r *reader_test)setcontent(contenth string){
+func (r *reader_test) setcontent(contenth string) {
 	r.content = contenth
 }
 
--- openapi/errors.go
+++ openapi/errors.go
@@ -1,13 +1,24 @@
-/*
- * Copyright (C) 1999-2019 Alibaba Group Holding Limited
- */
+// Copyright 1999-2019 Alibaba Group Holding Limited
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package openapi
 
 import (
 	"fmt"
+	"strings"
+
 	"github.com/aliyun/aliyun-cli/cli"
 	"github.com/aliyun/aliyun-cli/meta"
-	"strings"
 )
 
 // return when use unknown product
