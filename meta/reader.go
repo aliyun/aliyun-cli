2cedce221c372eff8829b381cb50b31449970ec4
Jackson Tian <shyvo1987@gmail.com>
2021-09-24 12:37:54 +0800

    cleanup: remove useless method for yaml

--- meta/reader.go
+++ meta/reader.go
@@ -16,24 +16,10 @@ package meta
 import (
 	"encoding/json"
 	"fmt"
-
-	"gopkg.in/yaml.v2"
 )
 
 type Reader interface {
 	ReadFrom(path string) ([]byte, error)
-}
-
-func ReadYamlFrom(r Reader, path string, v interface{}) error {
-	buf, err := r.ReadFrom(path)
-	if err != nil {
-		return fmt.Errorf("read yaml from %s failed %v", path, v)
-	}
-	err = yaml.Unmarshal(buf, v)
-	if err != nil {
-		return fmt.Errorf("unmarshal yaml %s failed %v", path, err)
-	}
-	return nil
 }
 
 func ReadJsonFrom(r Reader, path string, v interface{}) error {
@@ -46,8 +32,4 @@ func ReadJsonFrom(r Reader, path string, v interface{}) error {
 		return fmt.Errorf("unmarshal json %s failed %v", path, err)
 	}
 	return nil
-}
-
-type FileReader struct {
-	//TODO GA-1
 }
