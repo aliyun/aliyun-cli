5fb3d8ac0457535797b9421a967c40012806b478
Jackson Tian <shyvo1987@gmail.com>
2023-10-12 14:12:29 +0800

    refine: improve coding style

--- openapi/pager.go
+++ openapi/pager.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//	http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -228,9 +228,7 @@ func (a *Pager) mergeCollections(body interface{}) error {
 	} else if ar == nil {
 		return fmt.Errorf("jmespath result empty: %s", a.collectionPath)
 	}
-	for _, i := range ar.([]interface{}) {
-		a.results = append(a.results, i)
-	}
+	a.results = append(a.results, ar.([]interface{})...)
 	return nil
 }
 
