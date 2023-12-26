e99a49dd91021c75c50c0cbd5e9173bb38723a83
Jackson Tian <shyvo1987@gmail.com>
2023-12-26 15:53:09 +0800

    lib: add description for api

--- newmeta/meta_test.go
+++ newmeta/meta_test.go
@@ -17,3 +17,15 @@ func TestGetMetadata(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Greater(t, len(content), 100)
 }
+
+func TestGetAPI(t *testing.T) {
+	api, err := GetAPI("en", "ecs", "DescribeRegions")
+	assert.Nil(t, err)
+	assert.Equal(t, "DescribeRegions", api.Title)
+	assert.Equal(t, "Queries available Alibaba Cloud regions.", api.Summary)
+	assert.Equal(t, false, api.Deprecated)
+
+	api2, err := GetAPI("en", "ecs", "Invalid")
+	assert.Nil(t, err)
+	assert.Nil(t, api2)
+}
