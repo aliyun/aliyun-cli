f4544a3c5973712e2ee72876933eaa606a485098
Jackson Tian <shyvo1987@gmail.com>
2024-07-15 23:54:45 +0800

    move UnquoteString to output_filter.go

--- openapi/output_filter_test.go
+++ openapi/output_filter_test.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//	http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -104,4 +104,9 @@ func TestTableOutputFilter_FormatTable(t *testing.T) {
 	str, err = tableout.FormatTable(rowpath, colNames, v)
 	assert.Equal(t, "Num | name  | type  | api\n--- | ----  | ----  | ---\n0   | <nil> | <nil> | <nil>\n1   | <nil> | <nil> | <nil>\n", str)
 	assert.Nil(t, err)
+}
+
+func TestUnquoteString(t *testing.T) {
+	str := UnquoteString(`"nicai"`)
+	assert.Equal(t, "nicai", str)
 }
