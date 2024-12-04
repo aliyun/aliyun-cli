8f485d1ed0824ba4af43bd9833140ce0ab1ee730
CodeSpaceiiii <oozhuming@163.com>
2024-12-04 10:05:47 +0800

    feat: add check and delete existing asset before upload build: update macOS version for release workflow

--- tools/upload_asset.sh
+++ tools/upload_asset.sh
@@ -19,8 +19,33 @@ RELEASE_ID=$(curl -fsSL \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/aliyun/aliyun-cli/releases/tags/"$TAG" | jq '.["id"]')
 
+if [ -z "$RELEASE_ID" ]; then
+  echo "Failed to get release ID for tag $TAG"
+  exit 1
+fi
+
+# 获取现有资产列表
+ASSET_ID=$(curl -fsSL \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $GITHUB_TOKEN" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/aliyun/aliyun-cli/releases/$RELEASE_ID/assets | jq -r ".[] | select(.name == \"$(basename "$ASSET")\") | .id")
+
+# 如果资产已存在，删除它
+if [ -n "$ASSET_ID" ]; then
+  echo "Asset already exists. Deleting asset ID $ASSET_ID"
+  curl -fsSL \
+    -X DELETE \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer $GITHUB_TOKEN" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    https://api.github.com/repos/aliyun/aliyun-cli/releases/assets/$ASSET_ID
+fi
+
+printf "Uploading %s to release %s\n" "$ASSET" "$RELEASE_ID"
+
 curl -fsSL \
-  -X POST \
+  -X PUT \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $GITHUB_TOKEN" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
